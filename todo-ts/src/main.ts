ac8a3e124b472ad7775346aa78cbb315ba680682
jeetkanodia <jeetkanodia@gmail.com>
2023-09-10 04:39:49 +0530

    add logic for new todo

---
+++ todo-ts/src/main.ts
@@ -0,0 +1,55 @@
+import './style.css'
+
+interface Todo{
+  title: string,
+  isCompleted: boolean,
+  readonly id: string
+}
+
+const todos:Todo[] = [];
+
+const todosContainer = document.querySelector(".todoContainer") as HTMLDivElement;
+
+const todoInput = document.getElementsByName("title")[0] as HTMLInputElement;
+
+const myForm = document.getElementById("myForm") as HTMLFormElement;
+
+myForm.onsubmit = (e: SubmitEvent) => {
+  e.preventDefault();
+
+  const todo:Todo = {
+    title: todoInput.value,
+    isCompleted: false,
+    id:String(Math.random()*1000)
+  };
+
+  todos.push(todo);
+  todoInput.value = "";
+  renderTodo(todos);
+};
+
+const generateTodoItem = (item:Todo) => {
+  const todo = document.createElement('div')
+  todo.className = "todo";
+
+  const checkBox = document.createElement('input')
+  checkBox.setAttribute('type' , 'checkbox');
+  checkBox.className='isCompleted'
+  checkBox.checked = item.isCompleted;
+
+  const paragraph = document.createElement('p')
+ paragraph.innerText = item.title
+  const btn = document.createElement('button')
+  btn.innerText='X'
+  btn.className='deleteBtn'
+
+  todo.append(checkBox , paragraph, btn);
+  todosContainer.append(todo)
+}
+
+const renderTodo = (todos: Todo[]) => {
+  todosContainer.innerText = "";
+  todos.forEach(item => {
+    generateTodoItem(item)
+  })
+}
